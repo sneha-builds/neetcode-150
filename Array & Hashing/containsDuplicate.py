d57fc125a3accf7a39a3f63ad4bdc8aa06e73b70
sneha-builds <sneharaj809@gmail.com>
2026-06-01 17:06:10 +0530

    Array : Conains Duplicate

---
+++ Array & Hashing/containsDuplicate.py
@@ -0,0 +1,13 @@
+# Problem: https://neetcode.io/problems/duplicate-integer/question
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                return True
+            seen.add(nums[i])
+        return False
