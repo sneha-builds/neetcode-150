0f39744925b6a90e15c7d243f549b39589c3dd20
sneha-builds <sneharaj809@gmail.com>
2026-06-01 17:09:35 +0530

    Array : Conains Duplicate

--- Array & Hashing/containsDuplicate.py
+++ Array & Hashing/containsDuplicate.py
@@ -1,4 +1,5 @@
-# Problem: https://neetcode.io/problems/duplicate-integer/question
+"""Problem:Given an integer array nums, return true if any value appears more than once in 
+ the array, otherwise return false."""
 # Time Complexity: O(n)
 # Space Complexity: O(n)
 
