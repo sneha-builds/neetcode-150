b416f3864ceaf9397ec2a59a4c051c0c2550bfb0
sneha-builds <sneharaj809@gmail.com>
2026-06-03 17:29:43 +0530

    group anagrams

---
+++ groupAnagram.py
@@ -0,0 +1,13 @@
+"""Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+"""
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            result[sortedS].append(s)
+        return list(result.values())
