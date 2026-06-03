866054143792b25b71dc0397e9e584cafb43d064
sneha-builds <sneharaj809@gmail.com>
2026-06-03 17:27:38 +0530

    valid anagram

---
+++ validAnagram.py
@@ -0,0 +1,8 @@
+"""Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false."""
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        return sorted(s)==sorted(t)
+        
