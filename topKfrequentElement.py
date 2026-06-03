b1aa7d9f28e65269fdefc44e60cbb70b26ab7537
sneha-builds <sneharaj809@gmail.com>
2026-06-03 17:30:52 +0530

    top k frequent element

---
+++ topKfrequentElement.py
@@ -0,0 +1,23 @@
+"""Given an integer array nums and an integer k, return the k most frequent elements within the array.
+
+The test cases are generated such that the answer is always unique.
+
+You may return the output in any order.
+
+"""
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        arr = []
+        for num, cnt in count.items():
+            arr.append([cnt, num])
+        arr.sort()
+
+        res = []
+        while len(res) < k:
+            res.append(arr.pop()[1])
+        return res
