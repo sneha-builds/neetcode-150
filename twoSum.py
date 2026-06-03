f576e7e935a6def4b6579c602038752da99d6411
sneha-builds <sneharaj809@gmail.com>
2026-06-03 17:28:49 +0530

    two sum

---
+++ twoSum.py
@@ -0,0 +1,19 @@
+"""Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+Return the answer with the smaller index first.
+
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+        return "not found"
+
+
+        
+
