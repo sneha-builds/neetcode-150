9bdba5d703676be2ebac88daa6b11f3f3c95807a
sneha-builds <sneharaj809@gmail.com>
2026-06-03 17:34:34 +0530

    merge two sorted linked list

---
+++ mergeTwoSortedLists.py
@@ -0,0 +1,20 @@
+"""You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
+
+The new list should be made up of nodes from list1 and list2.
+
+"""
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        if list1.val <=  list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
