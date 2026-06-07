ec9998d9949f67f1453a35af5310a1a1f2ba9a97
sneha-builds <sneharaj809@gmail.com>
2026-06-07 06:21:52 +0530

    Tapping Rain Water

---
+++ Two Pointers/tappingRainWater.py
@@ -0,0 +1,23 @@
+"""You are given an array of non-negative integers height which represent an elevation map. Each value height[i] represents the height of a bar, which has a width of 1.
+
+Return the maximum area of water that can be trapped between the bars.
+
+"""
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+        n = len(height)
+        res = 0
+
+        for i in range(n):
+            leftMax = rightMax = height[i]
+
+            for j in range(i):
+                leftMax = max(leftMax, height[j])
+            for j in range(i + 1, n):
+                rightMax = max(rightMax, height[j])
+
+            res += min(leftMax, rightMax) - height[i]
+        return res
