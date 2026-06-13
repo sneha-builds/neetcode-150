f5ecde928cbc33b0f0249984c8489c43b18cf752
sneha-builds <sneharaj809@gmail.com>
2026-06-13 17:11:18 +0530

    LL :Remove Nth Index from the end opf the list

---
+++ Linked List/removeNthIndeFromEndOfList.py
@@ -0,0 +1,16 @@
+"""Given the head of a linked list and an integer n, remove the nth node from the end of the list and return its head."""
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        nodes = []
+        current = head
+        while current:
+            nodes.append(current)
+            current= current.next
+
+        rmIndex = len(nodes) - n
+        if rmIndex == 0:
+            return head.next
+
+        nodes[rmIndex - 1].next = nodes[rmIndex].next
+        return head
