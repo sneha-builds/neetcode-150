d8a91ecaba78872cdacdda354d48bbd399330bb5
sneha-builds <sneharaj809@gmail.com>
2026-06-14 14:47:14 +0530

    1 D Dynamic programming: Climbing Stairs

---
+++ 1DdynamicProgramming/climbingStairs.py
@@ -0,0 +1,21 @@
+"""You are given an integer n representing the number of steps to reach the top of a staircase. You can climb with either 1 or 2 steps at a time.
+
+Return the number of distinct ways to climb to the top of the staircase.
+
+"""
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+               
+        ways_to_n_minus_2 = 1
+        ways_to_n_minus_1 = 2
+        
+        for i in range(3, n + 1):
+            current_ways = ways_to_n_minus_1 + ways_to_n_minus_2
+            
+            ways_to_n_minus_2 = ways_to_n_minus_1
+            ways_to_n_minus_1 = current_ways
+            
+        return ways_to_n_minus_1
