08613e7b2380c7714ebed7b46369a4b220adcc42
sneha-builds <sneharaj809@gmail.com>
2026-06-18 21:56:17 +0530

    LL: Copy Linked List with random pointer

---
+++ Linked List/copyLLWith RandomNum.py
@@ -0,0 +1,35 @@
+#Copy Linked List with Random Pointer
+
+"""
+You are given the head of a linked list of length n. Unlike a singly linked list, each node contains an additional pointer random, which may point to any node in the list, or null.
+Create a deep copy of the list.
+The deep copy should consist of exactly n new nodes, each including:
+The original value val of the copied node
+A next pointer to the new node corresponding to the next pointer of the original node
+A random pointer to the new node corresponding to the random pointer of the original node
+Note: None of the pointers in the new list should point to nodes in the original list.
+Return the head of the copied linked list.
+In the examples, the linked list is represented as a list of n nodes.
+ Each node is represented as a pair of [val, random_index] where random_index is the index of the node (0-indexed) that the random pointer points to, or null if it does not point to any node.
+ """
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+        
+        old_to_new = {}
+
+        curr = head
+        while curr:
+            old_to_new[curr] = Node(curr.val)
+            curr = curr.next
+
+        curr = head
+        while curr:
+
+            old_to_new[curr].next = old_to_new.get(curr.next)
+            old_to_new[curr].random = old_to_new.get(curr.random)
+            curr = curr.next
+        
+        return old_to_new[head]
