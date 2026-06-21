a98f28c9e6d7d4ba4ac3a87d3bb11e7ba9f59284
sneha-builds <sneharaj809@gmail.com>
2026-06-21 14:04:49 +0530

    Sliding Window: Longest Repeating Character Replacement

---
+++ Sliding Window/longestRepeatingCharacterReplacement.py
@@ -0,0 +1,18 @@
+# Longest Repeating Character Replacement
+
+"""
+You are given a string s consisting of only uppercase english characters and an integer k. You can choose up to k characters of the string and replace them with any other uppercase English character.
+After performing at most k replacements, return the length of the longest substring which contains only one distinct character.
+"""
+
+class Solution:
+    def characterReplacement(self, s : str, k: int) -> int:
+        res = 0
+        for i in range(len(s)):
+            count, maxf = {}, 0
+            for j in range(i, len(s)):
+                count[s[j]] = 1 + count.get(s[j], 0)
+                maxf = max(maxf, count[s[j]])
+                if (j - i + 1) - maxf <= k:
+                    res = max(res, j - i + 1)
+        return res
