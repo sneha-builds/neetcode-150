2f6343ad5d7480496ecf45289677011e4d7c5744
sneha-builds <sneharaj809@gmail.com>
2026-06-26 21:28:54 +0530

    Binary Search : Search a 2D Matrix

---
+++ Binary Search/search2dMatrix.py
@@ -0,0 +1,27 @@
+# Search a 2D Matrix
+
+"""
+
+You are given an m x n 2-D integer array matrix and an integer target.
+
+Each row in matrix is sorted in non-decreasing order.
+The first integer of every row is greater than the last integer of the previous row.
+Return true if target exists within matrix or false otherwise.
+
+Can you write a solution that runs in O(log(m * n)) time?
+
+"""
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        r, c = 0, n - 1
+
+        while r < m and c >= 0:
+            if matrix[r][c] > target:
+                c -= 1
+            elif matrix[r][c] < target:
+                r += 1
+            else:
+                return True
+        return False
