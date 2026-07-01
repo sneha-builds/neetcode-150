b40f37c3963ffe1ba7aabd4977d2e4bad484c4ec
sneha-builds <sneharaj809@gmail.com>
2026-07-01 14:58:00 +0530

    Trees: Diameter of Binary Tree

--- Trees/diameterOfBinaryTree.py
+++ Trees/diameterOfBinaryTree.py
@@ -1,7 +1,6 @@
 # Diameter of Binary Tree
 
 """
-
 The diameter of a binary tree is defined as the length of the longest path between any two nodes within the tree. The path does not necessarily have to pass through the root.
 
 The length of a path between two nodes in a binary tree is the number of edges between the nodes. Note that the path can not include the same node twice.
@@ -15,7 +14,6 @@ Output: 3
 Exapmle 2:
 Input: root = [1,2,3]
 Output: 2
-
 """
 
 class Solution:
