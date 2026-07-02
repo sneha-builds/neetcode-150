d41d2d161ddd79747d621078f7e581fee69cc307
sneha-builds <sneharaj809@gmail.com>
2026-07-02 21:11:45 +0530

    Trees: Balanced Binary Tree

---
+++ Trees/balancedBinaryTree.py
@@ -0,0 +1,28 @@
+# Balanced Binary Tree
+
+"""
+Given a binary tree, return true if it is height-balanced and false otherwise.
+
+A height-balanced binary tree is defined as a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+Example 1:
+Input: root = [1,2,3,null,null,4]
+Output: true
+
+Constraints:
+The number of nodes in the tree is in the range [0, 1000].
+-1000 <= Node.val <= 1000
+"""
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root:
+                return [True, 0]
+
+            left, right = dfs(root.left), dfs(root.right)
+            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+            return [balanced, 1 + max(left[1], right[1])]
+
+        return dfs(root)[0]
+        
