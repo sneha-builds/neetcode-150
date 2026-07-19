8f33928fd56e656c69676d4d85782a201799033b
sneha-builds <sneharaj809@gmail.com>
2026-07-19 20:58:06 +0530

    1D Dynamic Programming : Min cost climbing stairs

---
+++ 1DdynamicProgramming/minCostClimbingStairs.py
@@ -0,0 +1,23 @@
+# Min Cost Climbing Stairs
+
+"""
+
+You are given an array of integers cost where cost[i] is the cost of taking a step from the ith floor of a staircase.
+After paying the cost, you can step to either the (i + 1)th floor or the (i + 2)th floor.
+
+You may choose to start at the index 0 or the index 1 floor.
+
+Return the minimum cost to reach the top of the staircase, i.e. just past the last index in cost.
+
+Constraints:
+
+  . 2 <= cost.length <= 100
+  . 0 <= cost[i] <= 100
+
+"""
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        for i in range(len(cost) -3, -1, -1):
+            cost[i] += min(cost[i +1], cost[i + 2])
+        return min(cost[0], cost[1])
